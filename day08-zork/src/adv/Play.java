4228b5d8778419df8483ea17dfb568e44af88ac3
Wee Seng <wee.bscl@gmail.com>
2022-12-14 20:53:46 +0800

    Added game loop in Play.java

--- day08-zork/src/adv/Play.java
+++ day08-zork/src/adv/Play.java
@@ -1,6 +1,7 @@
 package adv;
 
 import java.io.Console;
+import java.util.Optional;
 
 public class Play {
 
@@ -12,9 +13,35 @@ public class Play {
 
     public void start() {
         Console cons = System.console();
+        String userInput;
+        Room currRoom = this.storyFile.getRooms().get(this.storyFile.getStartRoom());
+        System.out.println(currRoom.getName() + "\n" + currRoom.getDescription());
 
         // Game loop
-
+        while (true) {
+            //
+            userInput = cons.readLine("> ").trim();
+            if (userInput.equalsIgnoreCase("exit")) {
+                // exit program
+                break;
+            } else if (userInput.equalsIgnoreCase("look")) {
+                // reprint description
+                System.out.println(currRoom.getName() + "\n" + currRoom.getDescription());
+            } else if (userInput.equalsIgnoreCase("directions")) {
+                // prints possible directions
+                System.out.println(currRoom.getDirections());
+            } else {
+                // try to get next room
+                Optional<String> opt = currRoom.go(userInput);
+                if (opt.isEmpty()) {
+                    System.out.println("You can't get there...");
+                } else {
+                    String r = opt.get();
+                    currRoom = this.storyFile.getRooms().get(r);
+                    System.out.println(currRoom.getName() + "\n" + currRoom.getDescription());
+                }
+            }
+        }
+        System.out.println("Exiting...");
     }
-    
 }
